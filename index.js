83ba0497cc18c3d7fbf025b59f7753157cdcbb1d
Khải <hvksmr1996@gmail.com>
2016-04-20 17:28:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 
 'use strict';
 
-var readdirSync = require('fs').readdirSync;
-var spawn = require('child_process').spawn;
+var chdir = require('process').chdir;
+var fs = require('fs');
+var child_process = require('child_process');
 class ChildProcessArray extends require('./child-process-array.js') {}
 
-const REPO_DIR = 'D:/JS_FILES/NodeJS/node_modules';
+var freeze = Object.freeze;
+var readdirSync = fs.readdirSync;
+var statSync = fs.statSync;
+var spawn = child_process.spawn;
 
-var pull = (callback) => {
+const REPO_DIR = 'D:/JS_FILES/NodeJS/node_modules';
+const CHECKOUT_ARGS = freeze('git', freeze(['checkout', 'origin', 'master']));
+const PULL_ARGS = freeze('git', freeze(['pull', 'origin', 'master']));
+
+var pull = () => {
 	var chprcs = new ChildProcessArray();
 	for (let dir of readdirSync(REPO_DIR)) {
-		// let childprc = spawn(
+		let currentpath = `${REPO_DIR}/${dir}`;
+		if (!statSync(currentpath).isDirectory()) {
+			continue;
+		}
+		if (!statSync(`${currentpath}/.git`).isDirectory()) {
+			continue;
+		}
+		chdir(currentpath);
+		chprcs.push(spawn(...PULL_ARGS));
 	}
+	return chprcs;
 };
 
-module.exports = pull;
+module.exports = () => pull();
