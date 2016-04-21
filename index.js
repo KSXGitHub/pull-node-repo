b4f1aa717492066078e50e56cf39409c52daa8a5
Khải <hvksmr1996@gmail.com>
2016-04-21 08:58:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 var chdir = require('process').chdir;
 var fs = require('fs');
 var child_process = require('child_process');
-class ChildProcessArray extends require('./child-process-array.js') {}
 
 var freeze = Object.freeze;
 var readdirSync = fs.readdirSync;
@@ -16,19 +15,6 @@ const CHECKOUT_ARGS = freeze('git', freeze(['checkout', 'origin', 'master']));
 const PULL_ARGS = freeze('git', freeze(['pull', 'origin', 'master']));
 
 var pull = () => {
-	var chprcs = new ChildProcessArray();
-	for (let dir of readdirSync(REPO_DIR)) {
-		let currentpath = `${REPO_DIR}/${dir}`;
-		if (!statSync(currentpath).isDirectory()) {
-			continue;
-		}
-		if (!statSync(`${currentpath}/.git`).isDirectory()) {
-			continue;
-		}
-		chdir(currentpath);
-		chprcs.push(spawn(...PULL_ARGS));
-	}
-	return chprcs;
 };
 
 module.exports = () => pull();
