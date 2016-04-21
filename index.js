66dd7445c68f2e0aed053a0f8f16c2726df8ecde
Khải <hvksmr1996@gmail.com>
2016-04-21 09:18:06 +0700

    index.js: complete result.checkout
    Promise would be rejected when `childprc` exit

--- index.js
+++ index.js
@@ -11,11 +11,28 @@ var readdirSync = fs.readdirSync;
 var statSync = fs.statSync;
 var spawn = child_process.spawn;
 
+var _getfunc = (fn, ...fnlist) =>
+	typeof fn === 'function' ? fn : _getfunc(...fnlist);
+
 const REPO_DIR = 'D:/JS_FILES/NodeJS/node_modules';
 const CHECKOUT_ARGS = freeze('git', freeze(['checkout', 'origin', 'master']));
 const PULL_ARGS = freeze('git', freeze(['pull', 'origin', 'master']));
+const DONOTHING = () => {};
 
-var pull = () => {
+var pull = (onspawn) => {
+	var steps = readdirSync(REPO_DIR).map((dirname) => {
+		var currdir = `${REPO_DIR}/${dirname}`;
+		chdir(dirname);
+		return {
+			checkout(resolve, reject) {
+				var childprc = spawn(...CHECKOUT_ARGS);
+				onspawn(childprc);
+				childprc.on('exit', (code, signal) => signal ? reject(signal) : resolve({childprc: childprc, code: code}));
+			},
+			pull() {},
+			__proto__: null
+		};
+	});
 };
 
-module.exports = () => pull();
+module.exports = (onspawn) => pull(_getfunc(onspawn, DONOTHING));
