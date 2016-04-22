2d3ca97c5cec2040841cea1c94bde8359b3abeeb
Khải <hvksmr1996@gmail.com>
2016-04-22 12:17:18 +0700

    Complete exec.js

--- exec.js
+++ exec.js
@@ -1,3 +1,20 @@
 #! /usr/bin/env node --es-staging
 
 'use strict';
+
+var process = require('process');
+var promise = require('.')(onspawn, onskip);
+
+function onspawn(event) {
+	console.log(event.type === 'checkout' ? `DIRECTORY "${event.dirname}"` : `PULLING`);
+	mkio(event.childprc);
+}
+
+function onskip(dirname) {
+	console.error(`SKIPPED "${dirname}"`);
+}
+
+function mkio(childprc) {
+	['stdout', 'stderr']
+		.forEach((stream) => childprc[stream].on('data', (chunk) => process[stream].write(chunk)));
+}
